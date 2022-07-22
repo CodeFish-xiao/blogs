8356892b86a52dfaae58745870f33a00252d41fd
肖肖 <codefish@peropero.net>
2022-07-22 14:19:19 +0800

    singleflight 添加超时控制

--- code/single/demo_test.go
+++ code/single/demo_test.go
@@ -57,3 +57,26 @@ func TestSingleFlightDemoDoChan(t *testing.T) {
 	wg.Wait()
 
 }
+func TestSingleFlightDemoForget(t *testing.T) {
+	var g singleflight.Group
+	var wg sync.WaitGroup
+	const n = 5
+	key := "something url(db)"
+	for i := 0; i < n; i++ {
+		wg.Add(1)
+		go func(j int) {
+			defer wg.Done()
+			v, _, shared := g.Do(key, func() (interface{}, error) {
+				go func() {
+					time.Sleep(10 * time.Millisecond)
+					fmt.Printf("Deleting key: %v\n", key)
+					g.Forget(key)
+				}()
+				ret, err := find(context.Background(), key)
+				return ret, err
+			})
+			fmt.Printf("index: %d, val: %v, shared: %v\n", j, v, shared)
+		}(i)
+	}
+	wg.Wait()
+}
