1e63e03d5b2cc319da89c1eb13a4b3f865f1fe53
肖肖 <codefish@peropero.net>
2022-07-22 11:51:24 +0800

    singleflight

--- code/single/demo.go
+++ code/single/demo.go
@@ -3,10 +3,12 @@ package single
 import (
 	"context"
 	"fmt"
+	"time"
 )
 
 type Result string
 
 func find(ctx context.Context, query string) (Result, error) {
+	time.Sleep(time.Second * 1)
 	return Result(fmt.Sprintf("result for %q", query)), nil
 }
--- code/single/demo_test.go
+++ code/single/demo_test.go
@@ -4,33 +4,25 @@ import (
 	"context"
 	"fmt"
 	"golang.org/x/sync/singleflight"
-	"sync/atomic"
+	"sync"
 	"testing"
-	"time"
 )
 
-func TestDemo(t *testing.T) {
+func TestSingleFlightDemo(t *testing.T) {
 	var g singleflight.Group
+	var wg sync.WaitGroup
 	const n = 5
-	waited := int32(n)
-	done := make(chan struct{})
 	key := "something url(db)"
 	for i := 0; i < n; i++ {
+		wg.Add(1)
 		go func(j int) {
+			defer wg.Done()
 			v, _, shared := g.Do(key, func() (interface{}, error) {
 				ret, err := find(context.Background(), key)
 				return ret, err
 			})
-			if atomic.AddInt32(&waited, -1) == 0 {
-				close(done)
-			}
 			fmt.Printf("index: %d, val: %v, shared: %v\n", j, v, shared)
 		}(i)
 	}
-
-	select {
-	case <-done:
-	case <-time.After(time.Second):
-		fmt.Println("Do hangs")
-	}
+	wg.Wait()
 }
