b882e4c651c337f71d866f453a7f16b5b535176c
肖肖 <codefish@peropero.net>
2022-08-09 16:18:29 +0800

    ceshi1

--- code/fuck_defer/defer.go
+++ code/fuck_defer/defer.go
@@ -1,1 +1,31 @@
 package fuck_defer
+
+import "fmt"
+
+type Entity struct {
+	fieldOne string
+}
+
+//定义的Option方法
+type Option func(e *Entity)
+
+func FieldOne(fieldOne string) Option {
+	return func(e *Entity) {
+		e.fieldOne = fieldOne
+	}
+}
+
+func (e *Entity) WithOptions(options ...Option) *Entity {
+	for _, option := range options {
+		option(e)
+	}
+	return e
+}
+func (e *Entity) Do() {
+	fmt.Println(e.fieldOne)
+}
+
+//e := &Entity{}
+//e.WithOptions(
+//    FieldOne("field one's value"),
+//)
--- code/fuck_defer/f_test.go
+++ code/fuck_defer/f_test.go
@@ -1,24 +1,12 @@
 package fuck_defer
 
 import (
-	"log"
 	"testing"
-	"time"
 )
 
 func TestName(t *testing.T) {
-
-	func() {
-		defer func() {
-			if p := recover(); p != nil {
-				log.Println(p)
-			}
-		}()
-		func() {
-			go func() {
-				panic("asd")
-			}()
-			time.Sleep(1 * time.Second)
-		}()
-	}()
+	e := &Entity{}
+	e.WithOptions(
+		FieldOne("field one's value"),
+	).Do()
 }
